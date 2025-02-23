bd25eab115d830c6bf61f07c381162a8c08f2353
ritik-bit-by-bit <ritikroshanyadav9696@gmail.com>
2025-02-23 22:26:52 +0530

    n

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,18 +1,13 @@
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
-// const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-// async function generateContent(prompt) {
-//     const result=await model.generateContent(prompt);
-//     return result.response.text();
-// }
-// module.exports=generateContent
 require('dotenv').config()
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-pro-exp-02-05" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-pro-exp-02-05",
+  systemInstruction:`you are a code reviwer who has expertise in developement.
+   You look for the code and find the problems and then suggest the solution to the developer.
+   You always try to find a way to make a code more efffecient and clean.`
+ });
 
 async function generateContent(prompt) {
   try {
